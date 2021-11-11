abb328355465d5b47fc571b5249aac3a154bc300
unknown <ermolov1337@gmail.com>
2021-11-11 17:53:40 +0300

    first-layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header"> <img src="https://logos.textgiraffe.com/logos/logo-name/Russia-designstyle-russia-m.png" /></header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://ezdili-znaem.com/wp-content/uploads/2020/06/spain-fuerteventura-01.jpg" />
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
